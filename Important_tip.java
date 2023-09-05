7114b69d52a2a99e8b7de992ca88eb166a0b2996
JOOHD <hddong728@naver.com>
2023-09-05 21:26:36 +0900

    file update

--- Important_tip.java
+++
@@ -1,8 +0,0 @@
-public class Important_tip {
-
-    /**
-     * 1. <String, Object> Object 를 사용하는 이유 최상위 타입 이기에 하위 타입들 모두 받기 위해 특히 JSON 유연한 데이터 형식 처리 쵝오.
-       2. RequiredArgsConstructor 는 Lombok 제공, @Autowired 사용 안해도 됨
-       3. @Autowired는 Spring에서 제공, 생성자 자동 생성.    
-     * /
-}
